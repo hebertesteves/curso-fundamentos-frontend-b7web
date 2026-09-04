e08ad5530fa6ab9a91a78e5c99967b3bdc10616c
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-03 23:17:52 -0300

    feat: sobe aula 07 (comentarios no codigo)

--- Fundamentos-Javascript/Modulo01/js/script.js
+++ Fundamentos-Javascript/Modulo01/js/script.js
@@ -5,11 +5,18 @@ let nome = "Hebert"
 let idade = 90
 console.log("Meu nome é " + nome + " e tenho " + idade + " anos.")
 
-let nome2 = "Nome";
-let idade2 = 90;
-let logado = true;
-let lista = ["ovo", "massa", "corante"];
-let pessoa = {nome: "Hebert", idade: 90};
+let nome2 = "Nome"; // String
+let idade2 = 90; // Number
+let logado = true; // Boolean
+let lista = ["ovo", "massa", "corante"]; // Array
+let pessoa = {nome: "Hebert", idade: 90}; // Object
 
 console.log(typeof logado);
+
+// este é um comentário de uma linha
+
+/*
+este é um comentário
+de múltiplas linhas
+*/
 
