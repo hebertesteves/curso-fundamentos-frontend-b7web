814dec66644c0b53a3b78cf45a5eef4ce310945e
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-04 08:29:22 -0300

    feat: sobe aula 10 (let, const e var)

---
+++ Fundamentos-Javascript/Modulo01/js/script2.js
@@ -0,0 +1,19 @@
+let nome = "Hebert";
+console.log(nome);
+
+nome = "Esteves";
+console.log(nome);
+
+/* Erro ao tenmtar alterar o valor de uma constante
+const nome = "Hebert";
+console.log(nome);
+
+nome = "Esteves";
+console.log(nome);
+*/
+
+var idade = 90;
+console.log(idade);
+
+idade = 99;
+console.log(idade);
