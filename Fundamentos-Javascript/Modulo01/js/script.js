9dd5b1d8b46293515f631b59284dd8f689d38690
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-03 23:13:03 -0300

    feat: sobe aula 06 (tipos de dados)

--- Fundamentos-Javascript/Modulo01/js/script.js
+++ Fundamentos-Javascript/Modulo01/js/script.js
@@ -4,3 +4,12 @@ console.log("Olá, mundo 2.0!")
 let nome = "Hebert"
 let idade = 90
 console.log("Meu nome é " + nome + " e tenho " + idade + " anos.")
+
+let nome2 = "Nome";
+let idade2 = 90;
+let logado = true;
+let lista = ["ovo", "massa", "corante"];
+let pessoa = {nome: "Hebert", idade: 90};
+
+console.log(typeof logado);
+
