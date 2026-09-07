ecbd43c9480dfce1eef062aff8d4f429aff3d8fe
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-06 21:24:47 -0300

    feat: sobe aula 14 (condicional == e ===)

--- Fundamentos-Javascript/Modulo01/js/script3.js
+++ Fundamentos-Javascript/Modulo01/js/script3.js
@@ -1,7 +1,13 @@
-let idade = 90;
+let idade = "20";
 
 if (idade >= 18) {
     console.log("Você é maior de idade!!");
 } else {
     console.log("Você é menor de idade!!");
+}
+
+if (idade === 20) {
+    idade = idade + 15;
+    console.log("Idade adicionada com suceso!");
+    console.log(idade);
 }
