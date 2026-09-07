d75c184e1a6bf81b70028a2e729d557367c8e02e
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-07 19:45:33 -0300

    feat: sobe aula 18 (exercicios condicional)

--- Fundamentos-Javascript/Modulo01/js/script3.js
+++ Fundamentos-Javascript/Modulo01/js/script3.js
@@ -42,3 +42,33 @@ if (adulto) {
 } else {
     console.log("Não é adulto");
 }
+
+// ================================================== //
+
+// Exercicio Condicional #2
+
+// Substituia o @ pela condicional correta para que o resultado seja TRUE
+let x = 10;
+let y = 5;
+console.log(y < x);
+
+// Substituia o @ pela condicional correta para que o resultado seja FALSE
+let w = "10";
+let z = 10;
+console.log (w === z);
+
+// Substituia o @ pela condicional correta para que o resultado seja TRUE
+let a = 10;
+let b = 5;
+console.log (a != b);
+
+// Crie uma condicional para verificar se o preço da
+// carne está barato ou caro. PS: Até 45 está barato
+let preco = 40.3;
+let estaBarato = preco <= 45;
+
+if (estaBarato) {
+    console.log("O preço da carne está barato!!");
+} else {
+    console.log("O preço da carne não está barato!!");
+}
