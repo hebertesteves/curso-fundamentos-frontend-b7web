2dece419038ec9526a02114885785c2923a9adb2
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-08 08:31:10 -0300

    feat: sobe aula 04 (passando parametros em funcoes)

--- Fundamentos-Javascript/Modulo02/js/script.js
+++ Fundamentos-Javascript/Modulo02/js/script.js
@@ -1,6 +1,21 @@
+/*
 function gravidade() {
     console.log("A gravidde do planeta é:");
     console.log(9.8);
 }
 
 gravidade();
+*/
+
+function somar(n1, n2) {
+    let soma = n1 + n2;
+    console.log("Resultado: " + soma);
+}
+
+somar(10, 7);
+
+function nomeCompleto(nome, sobrenome) {
+    console.log(`Nome completo: ${nome} ${sobrenome}`);
+}
+
+nomeCompleto("Hebert", "Esteves");
