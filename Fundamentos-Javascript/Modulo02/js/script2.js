a93f9ad14005a1310f1f7a06240980a1531f0775
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-12 18:58:35 -0300

    feat: sobe aula 13 (introducao array)

---
+++ Fundamentos-Javascript/Modulo02/js/script2.js
@@ -0,0 +1,16 @@
+let colors = ["blue", "red", "green"];
+let idades = [10, 20, 30];
+
+console.log(colors);
+console.log(colors[1]);
+console.log(idades[2]);
+
+let lista = [
+    "algo", 
+    30, 
+    "blabla", 
+    true, 
+    ["xx", "yy"]
+];
+
+console.log(lista[4][0]);
