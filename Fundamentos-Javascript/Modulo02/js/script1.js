5020c25347a9e5bb2332bb180f96f2c801c12b91
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-11 23:43:21 -0300

    feat: sobe aula 11 (variaveis dentro de funcoes)

--- Fundamentos-Javascript/Modulo02/js/script1.js
+++ Fundamentos-Javascript/Modulo02/js/script1.js
@@ -7,3 +7,15 @@ function somar(x, y) {
 const somar = (x, y) => x + y;
 
 console.log(somar(10, 5));
+
+let count = 0;
+
+function add() {
+    // let count = 0;
+    count += 10;
+}
+
+add();
+add();
+
+console.log(count)
