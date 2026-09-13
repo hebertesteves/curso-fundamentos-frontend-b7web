ca65e71609c0a2cc9f2438cfa332b2353f5b5dbc
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-13 19:34:59 -0300

    feat: sobe aula 16 (o que sao objetos)

---
+++ Fundamentos-Javascript/Modulo02/js/script3.js
@@ -0,0 +1,15 @@
+let personagem = {
+    nome: "Hebert",
+    idade: 90,
+    pais: "Brasil",
+    olhos: ["preto", "azul"],
+    atributos: {
+        forca: 100,
+        magia: 5,
+        stamina: 15
+    }
+};
+
+console.log(`${personagem.nome} tem ${personagem.idade} anos.`);
+console.log(personagem.atributos.magia);
+console.log(personagem.olhos[1]);
