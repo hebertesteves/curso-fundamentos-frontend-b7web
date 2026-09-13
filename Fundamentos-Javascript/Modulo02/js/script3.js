dd3d593c1bf81338018e7221a8cb198d71e279cc
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-13 19:41:29 -0300

    feat: sobe aula 17 (acessando e alterando objetos)

--- Fundamentos-Javascript/Modulo02/js/script3.js
+++ Fundamentos-Javascript/Modulo02/js/script3.js
@@ -10,6 +10,24 @@ let personagem = {
     }
 };
 
+personagem.nome = "Fulano";
+personagem.atributos.forca += 5;
+personagem.olhos.push("verde")
+
 console.log(`${personagem.nome} tem ${personagem.idade} anos.`);
 console.log(personagem.atributos.magia);
 console.log(personagem.olhos[1]);
+console.log(`Força: ${personagem.atributos.forca}`);
+console.log(personagem.olhos);
+
+let personagem1 = {
+    nome: "Fulano",
+    idade: 90,
+    carros: [
+        { modelo: "BMW", cor: "Branco" },
+        { modelo: "Fiat", cor: "Preto"}
+    ]
+};
+
+console.log(personagem1.carros[0].cor);
+console.log(personagem1.carros[1].modelo);
