b046bd9a48d62436f0ee9f811384c2b6b6fc0a6c
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-14 08:32:01 -0300

    feat: sobe aula 20 (loop em arrays)

--- Fundamentos-Javascript/Modulo02/js/script4.js
+++ Fundamentos-Javascript/Modulo02/js/script4.js
@@ -1,3 +1,29 @@
-for(let n = 1; n <= 10; n++) {
-    console.log("Frase " + n)
+// let cores = ["preto", "branco", "azul", "vermelho"];
+let cores = [
+    { nome: "Preto", qt: 10 },
+    { nome: "Azul", qt: 5 },
+    { nome: "Vermelho", qt: 15 }
+]
+
+/*
+for (let n = 0; n < cores.length; n++) {
+    console.log(cores[n])
+}
+    */
+
+/*
+for (let i in cores) {
+    console.log(cores[i]);
+}
+*/
+
+/*
+for (let cor of cores) {
+    console.log(cor);
+}
+*/
+
+for (let cor of cores) {
+    cor.nome = cor.nome.toUpperCase();
+    console.log(`Nome: ${cor.nome} - ${cor.qt}`);
 }
