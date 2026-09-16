a1d3b046e1d8ccbc01979cb15ec796b3076e005c
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-16 20:27:00 -0300

    feat: sobe aula 24 (ordenacao de array)

--- Fundamentos-Javascript/Modulo02/js/script5.js
+++ Fundamentos-Javascript/Modulo02/js/script5.js
@@ -1,10 +1,33 @@
 let fruits = ["Maça", "Uva", "Laranja", "Banana"];
-fruits.pop(); // Remove o ultimo
-fruits.push("Kiwi");
-fruits.shift(); // Remove o primerio
-fruits[fruits.length - 1] = "Pêra";
+// fruits.pop(); // Remove o ultimo
+// fruits.push("Kiwi");
+// fruits.shift(); // Remove o primerio
+// fruits[fruits.length - 1] = "Pêra";
 
+// console.log(fruits);
+// console.log(fruits.length);
+
+// console.log(fruits.join(', '));
+
+fruits.sort();
+fruits.reverse();
 console.log(fruits);
-console.log(fruits.length);
 
-console.log(fruits.join(', '));
+let cars = [
+    { brand: "Fiat", year: 2022},
+    { brand: "BMW", year: 2018},
+    { brand: "Ferrari", year: 2020}
+]
+
+cars.sort((a, b) => {
+    // if (a.year > b.year) {
+    //     return 1;
+    // } else if (a.year < b.year) {
+    //     return -1
+    // } else {
+    //     return 0;
+    // }
+    return a.year - b.year
+});
+
+console.log(cars);
