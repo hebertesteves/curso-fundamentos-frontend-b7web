16e99dfc1d6a6518e6b9fc996a7b1aceefe21a56
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-18 22:02:02 -0300

    feat: sobe aula 03 (selecionando elementos 2)

--- Fundamentos-Javascript/Modulo03/js/script.js
+++ Fundamentos-Javascript/Modulo03/js/script.js
@@ -2,3 +2,15 @@
 
 let elementosH1 = document.getElementsByTagName("h1");
 console.log(elementosH1[0]); // Vai retornar <h1>Olá Javascript</h1>
+
+let divTeste = document.getElementById("teste");
+console.log(divTeste);
+
+elementosComClasseBotao = document.getElementsByClassName("botao");
+console.log(elementosComClasseBotao);
+
+let divTeste2 = document.querySelector("#teste2");
+console.log(divTeste2);
+
+let listaLiComIdTeste = document.querySelectorAll("#teste ul li");
+console.log(listaLiComIdTeste);
