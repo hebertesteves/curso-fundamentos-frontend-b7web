ae16f93bbeebab91e108d630a6d93d3b8787d3bf
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-20 20:28:15 -0300

    feat: sobe aula 09 (manipulando o css)

--- Fundamentos-Javascript/Modulo03/js/script.js
+++ Fundamentos-Javascript/Modulo03/js/script.js
@@ -85,3 +85,10 @@ botao.addEventListener("click", () => {
     clicou();
 });
 */
+
+function fazerAcao() {
+    const li = document.querySelector("li");
+    
+    li.style.backgroundColor = "#00F";
+    li.style.fontSize = "20px";
+}
