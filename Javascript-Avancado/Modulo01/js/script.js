3ae6c279091bdf63f1e11f191209c4ecd3561f6b
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-09-23 19:58:19 -0300

    feat: sobe aula 02 (POO no Javascript)

--- Javascript-Avancado/Modulo01/js/script.js
+++ Javascript-Avancado/Modulo01/js/script.js
@@ -1,1 +1,8 @@
 // OO = Orientação a Objetos
+
+// Programação Orientada a Objetos (POO - OOP)
+// Programação Procedural
+// Programação Funcional (PF - FP) (Functional Programming Paradigm)
+
+// CLASSES
+// FUNÇÕES/OBJETOS
